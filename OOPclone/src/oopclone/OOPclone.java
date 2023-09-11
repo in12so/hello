9069772bc985b2dcebf24f6d0d460f576ba18119
in12so <73948915+in12so@users.noreply.github.com>
2023-09-11 23:22:06 +0100

    hello added

---
+++ OOPclone/src/oopclone/OOPclone.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oopclone;
+
+/**
+ *
+ * @author IN12SO
+ */
+public class OOPclone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is a new text");
+    }
+    
+}
