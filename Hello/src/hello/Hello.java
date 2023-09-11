ab355f62ccb6d2e60cb9d85bd8cadaa349d23c30
in12so <73948915+in12so@users.noreply.github.com>
2023-09-11 23:31:10 +0100

    hello added

---
+++ Hello/src/hello/Hello.java
@@ -0,0 +1,18 @@
+
+package hello;
+
+/**
+ *
+ * @author IN12SO
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        System.out.println("HelloWorld");
+    }
+    
+}
